43561bc52dab2e2c27be048a8f883c1378fe645f
Amanda Azallia <mandazal123@gmail.com>
2017-09-12 14:35:29 +0700

    example JSONParsing

---
+++ app/src/main/java/com/example/android/jsonparser/ContactModel.java
@@ -0,0 +1,11 @@
+package com.example.android.jsonparser;
+
+import java.io.Serializable;
+
+/**
+ * Created by Adroady on 9/12/2017.
+ */
+
+public class ContactModel implements Serializable{
+    private string
+}
---
+++ app/src/main/java/com/example/android/jsonparser/JSONParseActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.jsonparser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JSONParseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jsonparse);
+    }
+}
